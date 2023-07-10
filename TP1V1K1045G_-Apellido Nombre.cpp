dcc14fbc51fc0ffd41abce929c1d10f8e7def005
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-07-10 07:11:22 -0300

    feature - LeeEsp()

--- TP1V1K1045G_-Apellido Nombre.cpp
+++ TP1V1K1045G_-Apellido Nombre.cpp
@@ -2,7 +2,38 @@
 
 using namespace std;
 
+const int nMaxEsp = 20;
+const int nChEsp = 20;
+
+typedef char str20[nChEsp+1];
+typedef str20 Esp[nMaxEsp];
+
+/*
+ * Lee una especialidad
+ * @return
+ * true: lectura exitosa
+ * false: lectura no exitosa | fin de archivo
+ */
+bool LeeEsp(ifstream &fEsp, str20 esp){
+    fEsp.get(esp, nChEsp);
+    fEsp.ignore();
+
+    return fEsp.good();
+}
+
 main() {
+
+    Esp vEsp;
+    ifstream fEsp("Especialidades.Txt");
+    int carEsp = 0;
+
+    while( LeeEsp(fEsp, vEsp[carEsp]) )
+        carEsp++;
+
+    cout << "Imprimiendo vector" << endl;
+    for(int i = 0; i < carEsp; i++){ //ciclaria de mas si pongo nMaxEsp
+        cout << i+1 << ". " << vEsp[i] << endl;
+    }
 
     return 0;
 }
