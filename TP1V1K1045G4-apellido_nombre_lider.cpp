80974b8af06032553bb5b1c94f01f02025f80acd
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-08-14 01:45:28 -0300

    add - performance reuciendo tamaño de memoria de variables add: Ordenamiento de medicos delete: funciones tester add: obtencion de nombre medico (sin testear) update: modularizacion de procSolicitud()

--- TP1V1K1045G4-apellido_nombre_lider.cpp
+++ TP1V1K1045G4-apellido_nombre_lider.cpp
@@ -8,7 +8,7 @@
     Curso: K1045
         Dias: Lunes, turno tarde.
     Grupo Nro.: 4
-    Integrantes: (Apellido, Nombre).
+    Integrantes: Cuello, Jeremias.
 */
 
 #include <fstream.h>
@@ -18,13 +18,13 @@
 using namespace std;
 
 // Especialidades
-const nMaxEsp = 20,
-      nChEsp = 20;
+const short nMaxEsp = 20,
+            nChEsp = 20;
 typedef char str20[nChEsp+1];
 typedef str20 vecEsp[nMaxEsp];
 
 //Medicos
-const nMaxMed = 60;
+const short nMaxMed = 60;
 struct regMed{
     str20 apeNom,
           esp;
@@ -33,9 +33,9 @@ struct regMed{
 typedef regMed vecMed[nMaxMed];
 
 //Turnos
-const nMaxSes = 24,
-      nMaxDias = 31,
-      nChObS = 15;
+const short nMaxSes = 24,
+            nMaxDias = 31,
+            nChObS = 15;
 typedef char str15[nChObS+1];
 struct Tur{
     str15 obra;
@@ -46,9 +46,9 @@ typedef Tur tenTur[nMaxEsp][nMaxSes][nMaxDias];
 //Turnos Previos
 struct regTur{
     str20 esp;
-    int dia,
-        hora,
-        minu;
+    short dia,
+          hora,
+          minu;
     Tur turno;
 };
 
@@ -56,7 +56,7 @@ struct regTur{
 struct regSlc{
     str20 apeNom,
           esp;
-    int dia;
+    short dia;
     char chTur;
     Tur turno;
 };
@@ -83,12 +83,49 @@ bool LeeMed(ifstream &fMed, regMed &rMed){
     fMed.ignore();
     fMed.get(rMed.esp, nChEsp+1);
     fMed.ignore();
-    fMed >> rMed.turno;
+    fMed.get(rMed.turno);
     fMed.ignore();
 
     return fMed.good();
 }
-void procMedicos(ifstream &fMed, vecMed vMed, int *carMed){
+void IntCmb(regMed &rMed1, regMed &rMed2){
+    regMed aux;
+
+    strcpy(aux.apeNom, rMed1.apeNom);
+    strcpy(aux.esp, rMed1.esp);
+    aux.turno = rMed1.turno;
+
+    strcpy(rMed1.apeNom, rMed2.apeNom);
+    strcpy(rMed1.esp, rMed2.esp);
+    rMed1.turno = rMed2.turno;
+
+    strcpy(rMed2.apeNom, aux.apeNom);
+    strcpy(rMed2.esp, aux.esp);
+    rMed2.turno = aux.turno;
+}
+void OrdxBur(vecMed vMed, short carMed){
+    short k = 0;
+    bool ordenado;
+
+    do{
+        k++;
+        ordenado = true;
+
+        for(short i = 0; i < carMed-k; i++){
+            if(strcmp(vMed[i].esp, vMed[i+1].esp) > 0){
+                ordenado = false;
+                IntCmb(vMed[i], vMed[i+1]);
+            }
+            if(strcmp(vMed[i].esp, vMed[i+1].esp) == 0){
+                if(vMed[i].turno > vMed[i+1].turno){
+                    ordenado = false;
+                    IntCmb(vMed[i], vMed[i+1]);
+                }
+            }
+        }
+    } while(!ordenado);
+}
+void procMedicos(ifstream &fMed, vecMed vMed, short *carMed){
     regMed rMed;
 
     *carMed = 0;
@@ -98,6 +135,8 @@ void procMedicos(ifstream &fMed, vecMed vMed, int *carMed){
         vMed[(*carMed)].turno = rMed.turno;
         (*carMed)++;
     }
+
+    OrdxBur(vMed, *carMed);
 }
 
 void IntCmb(str20 esp1, str20 esp2){
@@ -106,20 +145,19 @@ void IntCmb(str20 esp1, str20 esp2){
     strcpy(esp1, esp2);
     strcpy(esp2, aux);
 }
-void OrdxBur(vecEsp vEsp, int carEsp){
-    int k = 0;
+void OrdxBur(vecEsp vEsp, short carEsp){
+    short k = 0;
     bool ordenado;
 
     do{
         k++;
         ordenado = true;
 
-        for(int i = 0; i <= carEsp-k-1; i++){
+        for(short i = 0; i <= carEsp-k-1; i++)
             if(strcmp(vEsp[i], vEsp[i+1]) > 0){
                 ordenado = false;
                 IntCmb(vEsp[i], vEsp[i+1]);
             }
-        }
     } while(!ordenado);
 }
 bool LeeEsp(ifstream &fEsp, str20 esp){
@@ -134,17 +172,17 @@ bool LeeEsp(ifstream &fEsp, str20 esp){
 
     return fEsp.good();
 }
-void procEspecialidad(ifstream &fEsp, vecEsp vEsp, int *carEsp){
+void procEspecialidad(ifstream &fEsp, vecEsp vEsp, short *carEsp){
     *carEsp = 0;
     while( LeeEsp(fEsp, vEsp[*carEsp]) )
         (*carEsp)++;
     OrdxBur(vEsp, *carEsp);
 }
 
-void inicTur(tenTur &tTurnos, int nMaxEsp, int nMaxSes, int nMaxDias){
-    for(int e = 0; e < nMaxEsp; e++)
-        for(int t = 0; t < nMaxSes; t++)
-            for(int d = 0; d < nMaxDias; d++){
+void inicTur(tenTur &tTurnos, short nMaxEsp, short nMaxSes, short nMaxDias){
+    for(short e = 0; e < nMaxEsp; e++)
+        for(short t = 0; t < nMaxSes; t++)
+            for(short d = 0; d < nMaxDias; d++){
                 strcpy(tTurnos[e][t][d].obra, "*");
                 tTurnos[e][t][d].cred = 0;
             }
@@ -168,8 +206,8 @@ bool LeeTur(ifstream &fTur, regTur &turno){
 
     return fTur.good();
 }
-busBin(vecEsp vEsp, str20 eClv, int ult){
-    int pri = 0,
+short busBin(vecEsp vEsp, str20 eClv, short ult){
+    short pri = 0,
         med;
 
     while(pri <= ult){
@@ -195,7 +233,7 @@ short cnvHhMm(short hhmm){
 
     return ++pos;
 }
-void procTurnos(ifstream &fTur, tenTur &tTur, int *carTur, vecEsp vEsp, int carEsp){
+void procTurnos(ifstream &fTur, tenTur &tTur, short *carTur, vecEsp vEsp, short carEsp){
     *carTur = 0;
     regTur rTur;
 
@@ -206,30 +244,7 @@ void procTurnos(ifstream &fTur, tenTur &tTur, int *carTur, vecEsp vEsp, int carE
         (*carTur)++;
     }
 }
-
-void tstMostrarTensor(tenTur tensor, vecEsp vEsp){
-    for(int a = 0; a < nMaxEsp; a++){
-        cout << "ESPECIALIDAD[" << a << "]: " << vEsp[a] << endl;
-        cout << "  |Obra social    |Creden" << endl;
-        for(int b = 0; b < nMaxSes; b++){
-            if(b < 10) cout << " ";
-            cout << b;
-            for(int c = 0; c < nMaxDias; c++){
-                cout << "|" << tensor[a][b][c].obra << "|";
-                cout << tensor[a][b][c].cred;
-            }
-            cout << endl;
-        } cout << endl << endl;
-    }
-}
-void tstMostrarEsp(vecEsp vEsp, int carEsp){
-    for(int i = 0; i < carEsp; i++){
-        cout << "[" << i << "] " << vEsp[i]<< "|" << endl;
-    }
-    cout << "carEsp = " << carEsp << endl;
-}
-
-void cnvPosHhMm(str5 &res, int n){
+void cnvPosHhMm(str5 &res, short n){
     switch(n){
         case 0: strcpy(res, " 8:00"); break;
         case 1: strcpy(res, " 8:30"); break;
@@ -257,7 +272,7 @@ void cnvPosHhMm(str5 &res, int n){
         case 23: strcpy(res,"19:30"); break;
     }
 }
-void impTurno(ofstream &fLst, str20 esp, int dia, int posHor, str15 obra, int cred){
+void impTurno(ofstream &fLst, str20 esp, short dia, short posHor, str15 obra, int cred){
     str5 hora;
     cnvPosHhMm(hora, posHor);
     fLst << esp << " ";
@@ -267,8 +282,8 @@ void impTurno(ofstream &fLst, str20 esp, int dia, int posHor, str15 obra, int cr
     fLst << setw(6) << cred;
     fLst << endl;
 }
-void lstTurnos(ofstream &fLst, tenTur tTur, int carTur, vecEsp vEsp, str11 titulo){
-    int i,j,k;
+void lstTurnos(ofstream &fLst, tenTur tTur, short carTur, vecEsp vEsp, str11 titulo){
+    short i,j,k;
 
     i = j = k = 0;
 
@@ -315,15 +330,47 @@ bool LeeSlc(ifstream &fSlc, regSlc &rSlc){
     return fSlc.good();
 }
 
-void procSolicitud(ifstream &fSlc, tenTur &tTur, int *carTur, vecEsp vEsp, int carEsp, ofstream &fLst){
-    regSlc rSlc;
-    short posHor;
-    str5 hora;
+char* Medico(vecMed vMed, str20 rSlcEsp, vecEsp vEsp, short carEsp){
+    short posMed;
 
+    posMed = 3*busBin(vEsp, rSlcEsp, carEsp);
+
+    switch(vMed[posMed].turno){
+        case 'N': posMed++; break;
+        case 'T': posMed += 2; break;
+    }
+
+    return vMed[posMed].apeNom;
+}
+
+void ImprimirTit(ofstream &fLst){
     fLst << "Listado de Solicitudes Actuales orden Cronologico" << endl;
     fLst << "Apellido Nombre      Nom.Obr. Soc.   Nro.Cred. Especialidad        Dia Turno Medico/a" << endl;
+}
+
+void grabaSlctd(ofstream &fLst, regSlc rSlc, short posHor, vecMed vMed, vecEsp vEsp, short carEsp){
+    str5 hora;
+
+    cnvPosHhMm(hora, posHor);
+    fLst << rSlc.apeNom << " ";
+    fLst << rSlc.turno.obra << "  ";
+    fLst << rSlc.turno.cred << "   ";
+    fLst << rSlc.esp << " ";
+    fLst << setw(2) << rSlc.dia << " ";
+    fLst << hora << " ";
+    //fLst << Medico(vMed, rSlc.esp, vEsp, carEsp);
+    fLst << endl;
+}
+
+void procSolicitud(ifstream &fSlc, tenTur &tTur, short *carTur, vecEsp vEsp, short carEsp, ofstream &fLst, vecMed vMed){
+    regSlc rSlc;
+    short posHor;
+
+    ImprimirTit(fLst);
 
     while(LeeSlc(fSlc, rSlc)){
+
+
         switch(rSlc.chTur){
             case 'M': posHor = 0; break;
             case 'T': posHor = 8; break;
@@ -332,20 +379,13 @@ void procSolicitud(ifstream &fSlc, tenTur &tTur, int *carTur, vecEsp vEsp, int c
 
         while(strcmp(tTur[busBin(vEsp, rSlc.esp, carEsp)][posHor][rSlc.dia-1].obra, "*"))
             posHor++;
+
         strcpy(tTur[busBin(vEsp, rSlc.esp, carEsp)][posHor][rSlc.dia-1].obra, rSlc.turno.obra);
         tTur[busBin(vEsp, rSlc.esp, carEsp)][posHor][rSlc.dia-1].cred = rSlc.turno.cred;
         (*carTur)++;
 
-        cnvPosHhMm(hora, posHor);
-        fLst << rSlc.apeNom << " ";
-        fLst << rSlc.turno.obra << "  ";
-        fLst << rSlc.turno.cred << "   ";
-        fLst << rSlc.esp << " ";
-        fLst << setw(2) << rSlc.dia << " ";
-        fLst << hora << " ";
-        fLst << "Med(esp, turno)" << endl; // falta la funcion que devuelve el nombre del medico en funcion de la especialidad y el turno
+        grabaSlctd(fLst, rSlc, posHor, vMed, vEsp, carEsp);
     }
-
     fLst << endl;
 }
 
@@ -356,9 +396,9 @@ main() {
     regTur rTur;
     tenTur tTur;
     vecMed vMed;
-    int *carEsp,
-        *carTur,
-        *carMed;
+    short *carEsp,
+          *carTur,
+          *carMed;
 
 //Abrir todos los archivos
     ifstream fMed("Medicos.Txt"),
@@ -371,7 +411,7 @@ main() {
     procEspecialidad(fEsp, vEsp, carEsp);
     procTurnos(fTur, tTur, carTur, vEsp, *carEsp);
     lstTurnos(fLst, tTur, *carTur, vEsp, "INICIAL");
-    procSolicitud(fSlc, tTur, carTur, vEsp, *carEsp, fLst);
+    procSolicitud(fSlc, tTur, carTur, vEsp, *carEsp, fLst, vMed);
     lstTurnos(fLst, tTur, *carTur, vEsp, "ACTUALIZADO");
 
 //cerrar todos los archivos
